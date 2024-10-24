0fc7027f5b135ffcac11d079bcd70a01fec25433
Stephanie Karlsson <stephanie@ratthjalpen.se>
2024-10-24 14:43:47 +0200

    adding profileimg and name to messages from others

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,18 +4,18 @@ import { LuSendHorizonal } from "react-icons/lu";
 
 
 const testMessages = [
-  { id: 1, text: 'Hej!', sender: 'other' },
+  { id: 1, text: 'Hej!', sender: 'other', username: 'Denise', img: 'denise.jpg'},
   { id: 2, text: 'Hur mår du?', sender: 'self' },
-  { id: 3, text: 'Jag mår bra, tack!', sender: 'other' },
+  { id: 3, text: 'Jag mår bra, tack!', sender: 'other', username: 'Denise', img: 'denise.jpg' },
   { id: 4, text: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum', sender: 'self' },
   { id: 5, text: 'Lorem ipsum Lorem ipsum Lorem ipsum', sender: 'self' },
-  { id: 6, text: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum', sender: 'other' },
+  { id: 6, text: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum', sender: 'other', username: 'Denise', img: 'denise.jpg' },
   { id: 7, text: 'Lorem ipsu ipsum', sender: 'self' },
-  { id: 8, text: 'Lorem ipsumLorem ipsum', sender: 'other' },
+  { id: 8, text: 'Lorem ipsumLorem ipsum', sender: 'other', username: 'Denise', img: 'denise.jpg' },
   { id: 9, text: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsum', sender: 'self' },
   { id: 10, text: 'Lorem  ipsum', sender: 'self' },
-  { id: 11, text: 'Lorem ipsum  ipsum', sender: 'other' },
-  { id: 12, text: 'ipsum', sender: 'other' },
+  { id: 11, text: 'Lorem ipsum  ipsum', sender: 'other', username: 'Denise', img: 'denise.jpg' },
+  { id: 12, text: 'ipsum', sender: 'other', username: 'Denise', img: 'denise.jpg' },
 ];
 
 export const Chat = () => {
@@ -45,8 +45,18 @@ useEffect(() => {
     <section className='chat-section'>
       <div className='messages-container'>
         {messages.map((msg) => (
-          <div key={msg.id} className={`message ${msg.sender}`}>
+            <div key={msg.id} className="message-wrapper">
+            <div className="message-info">
+              {msg.sender === 'other' && (
+                <>
+                  <img src={msg.img} alt='' className="profile-image" />
+                  <p className="username">{msg.username}</p>
+                </>
+              )}
+            </div>
+            <div className={`message ${msg.sender}`}>
               {msg.text}
+            </div>
           </div>
         ))}
         <div ref={endOfMessagesRef} />
