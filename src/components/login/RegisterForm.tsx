aacc35ea4b88721b4fbb9968bef6066f08229a1e
Stephanie Karlsson <stephanie@ratthjalpen.se>
2024-11-07 14:38:04 +0100

    frontend validation on registerform

--- src/components/login/RegisterForm.tsx
+++ src/components/login/RegisterForm.tsx
@@ -1,34 +1,45 @@
+import '../../css/registerForm.css'
 import { createUser } from "../../api/userApi";
 import "../../css/signinForm.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchUsers } from "../../api/userApi";
+import { validatePassword } from '../../functions/userFunctions';
 
 export function RegisterForm({ toggleMode }: { toggleMode: () => void }) {
  
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
-  const [error, setError] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
   const [message, setMessage] = useState("")
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
+  const [errorUsername, setErrorUsername] = useState("");
+
 
   const handleRegister = async (e: React.FormEvent): Promise<void> => {
     e.preventDefault();
     setIsLoading(true);
 
+    const passwordValidationError = validatePassword(password);
+    if (passwordValidationError) {
+      setErrorPassword(passwordValidationError);
+      setIsLoading(false);
+      return;
+    }
+
     const checkUsers = await fetchUsers()
 
     if (password !== confirmPassword) {
-      setError("Passwords do not match.");
+      setErrorPassword("Passwords do not match.");
       setIsLoading(false);
       return;
     }
 
     const usernameExists = checkUsers.some(user => user.username === username);
     if (usernameExists) {
-      setError("Username unavailable.");
+      setErrorUsername("Username is already taken.");
       setIsLoading(false);
       return;
     }
@@ -49,16 +60,16 @@ export function RegisterForm({ toggleMode }: { toggleMode: () => void }) {
         toggleMode()
         navigate("/");
       }, 1500);
-     
 
     } catch (error) {
       setIsLoading(false);
-      setError("Something went wrong. Please try again later.");
+      setErrorPassword("Password not valid");
       console.error(error);
 
     } finally {
       setIsLoading(false);
-      setError('')
+      setErrorPassword('')
+      setErrorUsername('')
     }
   }
 
@@ -66,7 +77,6 @@ export function RegisterForm({ toggleMode }: { toggleMode: () => void }) {
   return (
     <form className="form-container" onSubmit={handleRegister}>
       <h2 className="register-header">Register</h2>
-      {error && <p className="error">{error}</p>}
       <div className="login-input-container">
         <label>Username</label>
         <input
@@ -76,8 +86,11 @@ export function RegisterForm({ toggleMode }: { toggleMode: () => void }) {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           required
-        />
-        <p className="register-help">8-20 characters</p>
+          />
+        <div className="username-help-container">
+          <p className="register-help">8-20 characters</p>
+          {errorUsername && <p className="error-username">{errorUsername}</p>}
+        </div>
       </div>
 
       <div className="login-input-container">
@@ -103,8 +116,9 @@ export function RegisterForm({ toggleMode }: { toggleMode: () => void }) {
           onChange={(e) => setConfirmPassword(e.target.value)}
           required
         />
+        {errorPassword && <p className="error-password">{errorPassword}</p>}
       </div>
-      {message && <p className="success">{message}</p>}
+      {message && <p className="success-message">{message}</p>}
       <button type="submit" className="signin-btn">
         {isLoading ? "Creating new user..." : "Sign up"}
       </button>
--- src/functions/userFunctions.ts
+++ src/functions/userFunctions.ts
@@ -5,3 +5,11 @@ export const filteredUsers = async (userId: string) => {
   const fetchedUsers = await fetchUsers();
   return fetchedUsers.filter((user: User) => user._id !== userId);
 };
+
+export const validatePassword = (password: string): string | null => {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!passwordRegex.test(password)) {
+    return "Password's not valid'";
+  }
+  return null; 
+};
